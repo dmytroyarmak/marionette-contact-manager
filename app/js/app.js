4ab93c8e44a48cd254c166fe1bffd93fd050df5c
Dmytro Yarmak <dyarmak@heliostech.hk>
2014-02-05 11:37:47 +0200

    Use Marionette.Application

--- app/js/app.js
+++ app/js/app.js
@@ -1,62 +1,69 @@
-window.ContactManager = {
+var ContactManager = new Marionette.Application({
   Models: {},
   Collections: {},
-  Views: {},
+  Views: {}
+});
 
-  start: function(data) {
-    var contacts = new ContactManager.Collections.Contacts(data.contacts),
-        router = new ContactManager.Router(),
-        mainRegion = new Marionette.Region({el: '.main-container'});
+ContactManager.addRegions({
+  mainRegion: '.main-container'
+});
 
-    router.on('route:home', function() {
-      router.navigate('contacts', {
-        trigger: true,
-        replace: true
-      });
+ContactManager.addInitializer(function(data) {
+  var contacts = new ContactManager.Collections.Contacts(data.contacts),
+      router = new ContactManager.Router();
+
+  router.on('route:home', function() {
+    router.navigate('contacts', {
+      trigger: true,
+      replace: true
+    });
+  });
+
+  router.on('route:showContacts', function() {
+    var contactsView = new ContactManager.Views.Contacts({
+      collection: contacts
     });
 
-    router.on('route:showContacts', function() {
-      var contactsView = new ContactManager.Views.Contacts({
-        collection: contacts
-      });
+    ContactManager.mainRegion.show(contactsView);
+  });
 
-      mainRegion.show(contactsView);
+  router.on('route:newContact', function() {
+    var newContactForm = new ContactManager.Views.ContactForm({
+      model: new ContactManager.Models.Contact()
     });
 
-    router.on('route:newContact', function() {
-      var newContactForm = new ContactManager.Views.ContactForm({
-        model: new ContactManager.Models.Contact()
+    newContactForm.on('form:submitted', function(attrs) {
+      attrs.id = contacts.isEmpty() ? 1 : (_.max(contacts.pluck('id')) + 1);
+      contacts.add(attrs);
+      router.navigate('contacts', true);
+    });
+
+    ContactManager.mainRegion.show(newContactForm);
+  });
+
+  router.on('route:editContact', function(id) {
+    var contact = contacts.get(id),
+        editContactForm;
+
+    if (contact) {
+      editContactForm = new ContactManager.Views.ContactForm({
+          model: contact
       });
 
-      newContactForm.on('form:submitted', function(attrs) {
-        attrs.id = contacts.isEmpty() ? 1 : (_.max(contacts.pluck('id')) + 1);
-        contacts.add(attrs);
+      editContactForm.on('form:submitted', function(attrs) {
+        contact.set(attrs);
         router.navigate('contacts', true);
       });
 
-      mainRegion.show(newContactForm);
-    });
+      ContactManager.mainRegion.show(editContactForm);
+    } else {
+      router.navigate('contacts', true);
+    }
+  });
+});
 
-    router.on('route:editContact', function(id) {
-      var contact = contacts.get(id),
-          editContactForm;
-
-      if (contact) {
-        editContactForm = new ContactManager.Views.ContactForm({
-            model: contact
-        });
-
-        editContactForm.on('form:submitted', function(attrs) {
-          contact.set(attrs);
-          router.navigate('contacts', true);
-        });
-
-        mainRegion.show(editContactForm);
-      } else {
-        router.navigate('contacts', true);
-      }
-    });
-
+ContactManager.on('initialize:after', function(options){
+  if (Backbone.history){
     Backbone.history.start();
   }
-};
+});
