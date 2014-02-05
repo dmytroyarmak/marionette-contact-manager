83d7620858326329fe9d1b4437a7547e0b270f08
Dmytro Yarmak <dyarmak@heliostech.hk>
2014-02-05 10:09:57 +0200

    Use ItemView in contactForm view

--- app/js/views/contactForm.js
+++ app/js/views/contactForm.js
@@ -1,25 +1,29 @@
-ContactManager.Views.ContactForm = Backbone.View.extend({
-  template: _.template($('#tpl-new-contact').html()),
+ContactManager.Views.ContactForm = Marionette.ItemView.extend({
+  template: '#tpl-new-contact',
+
+  ui: {
+    nameInput: '.contact-name-input',
+    telInput: '.contact-tel-input',
+    emailInput: '.contact-email-input'
+  },
 
   events: {
     'submit .contract-form': 'onFormSubmit'
   },
 
-  render: function() {
-    var html = this.template(_.extend(this.model.toJSON(), {
+  serializeData: function() {
+    return _.extend(this.model.toJSON(), {
       isNew: this.model.isNew()
-    }));
-    this.$el.append(html);
-    return this;
+    });
   },
 
   onFormSubmit: function(e) {
     e.preventDefault();
 
     this.trigger('form:submitted', {
-      name: this.$('.contact-name-input').val(),
-      tel: this.$('.contact-tel-input').val(),
-      email: this.$('.contact-email-input').val()
+      name: this.ui.nameInput.val(),
+      tel: this.ui.telInput.val(),
+      email: this.ui.emailInput.val()
     });
   }
 });
